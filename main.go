888947c47b83917cd26b8ba5f65ba47637eef2a8
progfay <shun1856shun@gmail.com>
2021-04-13 23:28:41 +0900

    feat: playground for extracting API endpoints

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strconv"
+	"strings"
+)
+
+func formatBundle(regexp, name string) string {
+	return fmt.Sprintf("[[bundle]]\nregexp = %q\nname = %q", regexp, name)
+}
+
+type Visitor struct {}
+
+func (v Visitor) Visit(node ast.Node) ast.Visitor {
+	callExpr, ok := node.(*ast.CallExpr)
+	if !ok {
+		return v
+	}
+
+	if len(callExpr.Args) < 1 {
+		return v
+	}
+
+	firstArg, ok := callExpr.Args[0].(*ast.BasicLit)
+	if !ok || firstArg.Kind != token.STRING {
+		return v
+	}
+	path, err := strconv.Unquote(firstArg.Value)
+	if err != nil || !strings.HasPrefix(path, "/") {
+		return v
+	}
+
+	selectorExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return v
+	}
+
+	name := selectorExpr.Sel.Name
+
+	fmt.Printf("%s %s\n", strings.ToUpper(name), path)
+	return v
+}
+
+func main() {
+    fset := token.NewFileSet()
+    f, _ := parser.ParseFile(fset, "./example/main.go", nil, parser.Mode(0))
+		v := Visitor{}
+
+		ast.Walk(v, f)
+}
