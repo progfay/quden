6c987974bdc352072a20760f6a5d35ba9d51ace1
progfay <shun1856shun@gmail.com>
2021-04-18 22:37:41 +0900

    test: add test for goji

---
+++ goji/goji_test.go
@@ -0,0 +1,80 @@
+package goji_test
+
+import (
+	"fmt"
+	"go/parser"
+	"reflect"
+	"testing"
+
+	"github.com/progfay/quden/endpoint"
+	"github.com/progfay/quden/goji"
+)
+
+func Test_MatchImportPath(t *testing.T) {
+	framework := goji.New()
+
+	for _, testcase := range []struct {
+		in   string
+		want bool
+	}{
+		{
+			in: "goji.io/pat",
+			want: true,
+		},
+		{
+			in: "fmt",
+			want: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("matching to %q", testcase.in), func(t *testing.T) {
+			got := framework.MatchImportPath(testcase.in)
+			if got != testcase.want {
+				t.Errorf("want %t, got %t", testcase.want, got)
+			}
+		})
+	}
+}
+
+func Test_NodeConverter_ToEndpoint(t *testing.T) {
+	framework := goji.New()
+
+	for _, testcase := range []struct {
+		name string
+		in   string
+		want *endpoint.Endpoint
+	}{
+		{
+			name: "Static Paths",
+			in: `pat.Get("/users")`,
+			want: endpoint.New("GET", "/users"),
+		},
+		{
+			name: "Named Matches",
+			in: `pat.Delete("/users/:user_id")`,
+			want: endpoint.New("DELETE", "/users/:user_id"),
+		},
+		{
+			name: "Prefix Matches",
+			in: `pat.Post("/users/files/*")`,
+			want: endpoint.New("POST", "/users/files/*"),
+		},
+		{
+			name: "Non API Endpoint Register",
+			in: `fmt.Println("/users")`,
+			want: nil,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			expr, err := parser.ParseExpr(testcase.in)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			got := framework.NewNodeConverter().ToEndpoint(expr)
+			if !reflect.DeepEqual(got, testcase.want) {
+				t.Errorf("want %q, got %q", testcase.want, got)
+			}
+		})
+	}
+}
