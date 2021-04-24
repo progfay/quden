1344c9a7d5a1632cc57148689b43579b71ac1fe7
progfay <shun1856shun@gmail.com>
2021-04-25 00:20:08 +0900

    test: add test for gorilla

--- gorilla/gorilla_test.go
+++ gorilla/gorilla_test.go
@@ -1,1 +1,104 @@
 package gorilla_test
+
+import (
+	"fmt"
+	"go/parser"
+	"go/token"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/progfay/quden/gorilla"
+	"github.com/progfay/quden/util"
+)
+
+const template = `package main
+
+import "github.com/gorilla/mux"
+
+func main() {
+	%s
+}
+`
+
+func Test_MatchImportPath(t *testing.T) {
+	framework := gorilla.New()
+
+	for _, testcase := range []struct {
+		in   string
+		want bool
+	}{
+		{
+			in:   "github.com/gorilla/mux",
+			want: true,
+		},
+		{
+			in:   "fmt",
+			want: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("matching to %q", testcase.in), func(t *testing.T) {
+			got := framework.MatchImportPath(testcase.in)
+			if got != testcase.want {
+				t.Errorf("want %t, got %t", testcase.want, got)
+			}
+		})
+	}
+}
+
+func Test_NodeConverter_ToEndpoint(t *testing.T) {
+	framework := gorilla.New()
+
+	for _, testcase := range []struct {
+		name string
+		in   []string
+		want []util.Endpoint
+	}{
+		{
+			name: "Static Paths",
+			in:   []string{`mux.NewRouter().HandleFunc("/users", handler).Methods("GET")`},
+			want: []util.Endpoint{util.NewEndpoint("GET", "/users", "/users")},
+		},
+		{
+			name: "Variable Paths",
+			in:   []string{`mux.NewRouter().HandleFunc("/users/{user_id}", handler).Methods("DELETE")`},
+			want: []util.Endpoint{util.NewEndpoint("DELETE", "/users/{user_id}", "/users/{user_id}")},
+		},
+		{
+			name: "Non API Endpoint Register",
+			in:   []string{`fmt.Println("/users")`},
+			want: nil,
+		},
+		{
+			name: "With 2 lines",
+			in:   []string{
+				`r := mux.NewRouter()`,
+				`r.HandleFunc("/users", handler).Methods("GET")`,
+			},
+			want: []util.Endpoint{util.NewEndpoint("GET", "/users", "/users")},
+		},
+		{
+			name: "Subrouter",
+			in:   []string{
+				`r := mux.NewRouter()`,
+				`l := r.PathPrefix("/users").Subrouter()`,
+				`l.Methods("GET").HandlerFunc(handler)`,
+			},
+			want: []util.Endpoint{util.NewEndpoint("GET", "/users", "/users")},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			source := fmt.Sprintf(template, strings.Join(testcase.in, "\n  "))
+			expr, err := parser.ParseFile(token.NewFileSet(), "", source, parser.Mode(0))
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			got := framework.Extract(expr)
+			if !reflect.DeepEqual(got, testcase.want) {
+				t.Errorf("want %q, got %q", testcase.want, got)
+			}
+		})
+	}
+}
